1a8d425bdb390b43a71863990cbf8ebe5aa8a9fa
Oussama Elhamraoui <oussama14elhamraoui@gamil.com>
2024-01-15 01:10:05 -0500

    commiting the decription and the simulation section in the algorithm sort page

--- public/script.js
+++ public/script.js
@@ -1,3 +1,40 @@
+const simulation = document.querySelector("[data-type='simulation']");
+const bars = simulation.querySelector(".bars");
+const arraySize = 10;
+let arr = [];
+function init(arr) {
+  for (let i = 0; i < arraySize; i++) {
+    arr[i] = Math.floor(Math.random() * 100);
+  }
+}
+function bubbleSort(arr) {
+  let swapped = false;
+  do {
+    for (let i = 0; i < arraySize; i++) {
+      if (arr[i - 1] > arr[i]) {
+        swapped = true;
+        [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+      }
+    }
+  } while (swapped);
+}
+function play(arr) {
+  init(arr);
+  bubbleSort(arr);
+  showBars(arr);
+}
+function showBars(arr) {
+  for (let i = 0; i < arraySize; i++) {
+    const bar = document.createElement("div");
+    bar.style.height = arr[i] + "%";
+    bar.style.width = 6 + "%";
+    bar.style.backgroundColor = "black";
+    bars.append(bar);
+  }
+}
+init(arr);
+bubbleSort(arr);
+
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("nav");
 
