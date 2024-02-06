38dd12acfc011f7f71292976396ec7a6027df197
Oussama Elhamraoui <oussama14elhamraoui@gamil.com>
2024-02-06 21:09:22 +0100

    edit

--- public/algorithms.js
+++ public/algorithms.js
@@ -12,3 +12,33 @@ function bubbleSort(arr) {
   } while (swapped);
   return swaps;
 }
+function insertionSort(arr) {
+  const swaps = [];
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      swaps.push([j - 1, j]);
+      [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+      j--;
+    }
+  }
+  return swaps;
+}
+function selectionSort(arr) {
+  const swaps = [];
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      swaps.push([i, minIndex]);
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return swaps;
+}
--- public/script.js
+++ public/script.js
@@ -1,12 +1,14 @@
 const sorts = document.querySelectorAll("[data-sort]");
 const dataBtn = document.querySelector(".data-btn");
 const dataBtns = dataBtn.querySelectorAll("li");
-const simulation = document.querySelector("[data-type='simulation']");
-const bars = simulation.querySelector(".bars");
-const buttons = simulation.querySelector(".buttons");
+const simulation1 = document.querySelector("[data-type='simulation-1']");
+const bars1 = simulation1.querySelector(".bars1");
+const buttons1 = simulation1.querySelector(".buttons1");
+const simulation2 = document.querySelector("[data-type='simulation-2']");
+const bars2 = simulation2.querySelector(".bars2");
+const buttons2 = simulation2.querySelector(".buttons2");
 const arraySize = 10;
 let arr = [];
-
 
 dataBtn.addEventListener("click", function (e) {
   const clicked = e.target.closest("li");
@@ -18,53 +20,63 @@ dataBtn.addEventListener("click", function (e) {
   clicked.classList.add("bg-white");
   clicked.classList.add("text-emerald-600");
 
-
   const dataTypes = clicked
     .closest(".sort-container")
     .querySelectorAll(`[data-type]`);
-
+  const dataNumbers = clicked
+    .closest(".sort-container")
+    .querySelectorAll(`[data-number]`);
+  let dataNumber;
+  for (let i = 0; i < dataNumbers.length; i++) {
+    if (!dataNumbers[i].classList.contains("hidden"))
+      dataNumber = dataNumbers[i];
+  }
   console.log(dataTypes);
 
   dataTypes.forEach((el) => el.classList.add("hidden"));
 
   const dataType = clicked
     .closest(".sort-container")
-    .querySelector(`[data-type=${clicked.dataset.typeClicked}]`);
+    .querySelector(
+      `[data-type=${clicked.dataset.typeClicked}-${dataNumber.dataset.number}]`
+    );
 
-  console.log("the value that is being cliked is : "+ clicked.dataset.typeClicked);
+  console.log(
+    "the value that is being cliked is : " + clicked.dataset.typeClicked
+  );
 
   dataType.classList.remove("hidden");
 
   console.log(dataType);
 });
 
-
 // Functions
-function init(arr) {
+function initBubble(arr) {
   for (let i = 0; i < arraySize; i++) {
     arr[i] = Math.floor(Math.random() * 100);
   }
-  showBars();
+  showBarsBubble();
 }
-function animate(swaps) {
+function animateBubble(swaps) {
   if (swaps.length == 0) {
-    showBars();
+    showBarsBubble();
     return;
   }
   const [i, j] = swaps.shift();
   [arr[i], arr[j]] = [arr[j], arr[i]];
-  showBars([i, j]);
+  showBarsBubble([i, j]);
   setTimeout(function () {
-    animate(swaps);
+    animateBubble(swaps);
   }, 300);
 }
-function play() {
+function playBubble() {
   const copyArr = [...arr];
   const swaps = bubbleSort(copyArr);
-  animate(swaps);
+  animateBubble(swaps);
 }
-function showBars(indexs) {
-  bars.innerHTML = "";
+console.log(bars1);
+function showBarsBubble(indexs) {
+  bars1.innerHTML = "";
   for (let i = 0; i < arraySize; i++) {
     const bar = document.createElement("div");
     bar.style.height = arr[i] + "%";
@@ -73,18 +85,73 @@ function showBars(indexs) {
     if (indexs && indexs.includes(i)) {
       bar.style.backgroundColor = "#059669";
     }
-    bars.append(bar);
+    bars1.append(bar);
   }
 }
 
 // DOM manipulation
-buttons.querySelector(".btn-init").addEventListener("click", function () {
-  init(arr);
-});
-buttons.querySelector(".btn-play").addEventListener("click", function () {
-  play(arr);
-});
-init(arr);
+buttons1
+  .querySelector(".btn-init-bubble")
+  .addEventListener("click", function () {
+    initBubble(arr);
+  });
+buttons1
+  .querySelector(".btn-play-bubble")
+  .addEventListener("click", function () {
+    playBubble(arr);
+  });
+initBubble(arr);
+
+function initInsertion(arr) {
+  for (let i = 0; i < arraySize; i++) {
+    arr[i] = Math.floor(Math.random() * 100);
+  }
+  showBarsInsertion();
+}
+function animateInsertion(swaps) {
+  if (swaps.length == 0) {
+    showBarsInsertion();
+    return;
+  }
+  const [i, j] = swaps.shift();
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+  showBarsInsertion([i, j]);
+  setTimeout(function () {
+    animateInsertion(swaps);
+  }, 300);
+}
+function playInsertion() {
+  const copyArr = [...arr];
+  const swaps = insertionSort(copyArr);
+  animateInsertion(swaps);
+}
+console.log(bars1);
+function showBarsInsertion(indexs) {
+  bars2.innerHTML = "";
+  for (let i = 0; i < arraySize; i++) {
+    const bar = document.createElement("div");
+    bar.style.height = arr[i] + "%";
+    bar.style.width = 6 + "%";
+    bar.style.backgroundColor = "black";
+    if (indexs && indexs.includes(i)) {
+      bar.style.backgroundColor = "#059669";
+    }
+    bars2.append(bar);
+  }
+}
+
+// DOM manipulation
+buttons2
+  .querySelector(".btn-init-insertion")
+  .addEventListener("click", function () {
+    initInsertion(arr);
+  });
+buttons2
+  .querySelector(".btn-play-insertion")
+  .addEventListener("click", function () {
+    playInsertion(arr);
+  });
+initInsertion(arr);
 
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("nav");
@@ -99,5 +166,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
-
